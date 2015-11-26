02159d53173fa728a4e37a22d91d62f1dbf569b3
pandersson06 <pontus_proffs06@hotmail.com>
2015-11-26 10:51:22 +0100

    Fix so it follows lab spec, made it simple but dummer

--- concurrentMessages.sh
+++ concurrentMessages.sh
@@ -1,3 +1,6 @@
-curl -d comment=test1 203.246.112.148:63163 &
-curl -d comment=test2 129.130.252.140:63163
-
+curl -d comment=test1 127.0.0.1:63100 &
+curl -d comment=test2 127.0.0.1:63101 &
+curl -d comment=test2 127.0.0.1:63100 &
+curl -d comment=test3 127.0.0.1:63102 &
+curl -d comment=test2 127.0.0.1:63103 &
+curl -d comment=test4 127.0.0.1:63101
