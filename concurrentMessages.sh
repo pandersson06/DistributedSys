50c58f67aeb558098d3de120f837b6a1c9acbd99
Filip Bertilsson <fibeube@gmail.com>
2015-11-26 12:40:35 +0100

    Small fix

--- concurrentMessages.sh
+++ concurrentMessages.sh
@@ -1,6 +1,4 @@
-curl -d comment=test1 127.0.0.1:63100 &
-curl -d comment=test2 127.0.0.1:63101 &
-curl -d comment=test2 127.0.0.1:63100 &
-curl -d comment=test3 127.0.0.1:63102 &
-curl -d comment=test2 127.0.0.1:63103 &
-curl -d comment=test4 127.0.0.1:63101
+curl -d comment=test1 148.206.185.34:63100 &
+curl -d comment=test2 130.217.77.4:63100 &
+curl -d comment=test3 128.220.231.4:63100 
+
