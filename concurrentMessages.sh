c19e25064f332e797a64537a6a5b00c9ac30d072
pandersson06 <pontus_proffs06@hotmail.com>
2015-11-29 17:09:00 +0100

    Working consistency with logic clock

--- concurrentMessages.sh
+++ concurrentMessages.sh
@@ -1,4 +1,5 @@
-curl -d comment=test1 148.206.185.34:63100 &
-curl -d comment=test2 130.217.77.4:63100 &
-curl -d comment=test3 128.220.231.4:63100 
-
+curl -d comment=test1 149.43.80.20:63163 &
+curl -d comment=test2 128.220.231.2:63163 &
+curl -d comment=test3 203.246.112.148:63163 &
+curl -d comment=test4 203.246.112.148:63163 &
+curl -d comment=test5 203.246.112.148:63163 &
---
+++ concurrentMessagesLocal.sh
@@ -0,0 +1,15 @@
+curl -d comment=test1 127.0.0.1:63100 &
+curl -d comment=test2 127.0.0.1:63100 &
+curl -d comment=test3 127.0.0.1:63100 &
+
+curl -d comment=test4 127.0.0.1:63101 &
+curl -d comment=test5 127.0.0.1:63101 &
+curl -d comment=test6 127.0.0.1:63101 &
+
+curl -d comment=test7 127.0.0.1:63102 &
+curl -d comment=test8 127.0.0.1:63102 &
+curl -d comment=test9 127.0.0.1:63102 &
+
+curl -d comment=test10 127.0.0.1:63103 &
+curl -d comment=test11 127.0.0.1:63103 &
+curl -d comment=test12 127.0.0.1:63103 &
