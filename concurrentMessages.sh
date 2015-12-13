3f014302e67f2d8317614b47952a73cbc4089216
pandersson06 <pontus_proffs06@hotmail.com>
2015-12-13 11:28:03 +0100

    Fixed so possible to run on seattle

--- concurrentMessages.sh
+++ concurrentMessages.sh
@@ -1,5 +1,3 @@
-curl -d comment=test1 149.43.80.20:63163 &
-curl -d comment=test2 128.220.231.2:63163 &
-curl -d comment=test3 203.246.112.148:63163 &
-curl -d comment=test4 203.246.112.148:63163 &
-curl -d comment=test5 203.246.112.148:63163 &
+curl -d comment=test1 129.32.84.160:63163/Add &
+curl -d comment=test2 192.16.125.11:63163/Add &
+curl -d comment=test3 156.56.250.227:63163/Add &
